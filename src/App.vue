ed948ec8e1b07a646dfe69664030e9cef05b8859
Jordan Gerdin <jordan.gerdin@gmail.com>
2020-05-09 15:16:09 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+    <div id="app">
+        <h1 class="title">Would You Rather...?</h1>
+        
+        <WouldYouRatherQuestion
+            v-for = "question in questions" 
+            v-bind:key="question.id"
+            v-bind:question="question.question"
+            v-on:answer-changed="questionAnswered"
+            v-bind:id = "question.id"
+            v-bind:answer1 = "question.answer1"
+            v-bind:answer2 = "question.answer2"
+            >
+        </WouldYouRatherQuestion>
+
+        <div class="" id="answers">
+            <h2>You chose...</h2>
+            <ul v-for = "answer in answers" 
+                v-bind:key="answer.index">
+                <li v-show = "answer">{{ answer }}</li>
+            </ul>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import WouldYouRatherQuestion from './components/WouldYouRatherQuestion.vue'
+
+export default {
+    name: 'app',
+    components: {
+        WouldYouRatherQuestion
+    },
+    data() {
+        return {
+            questions: [
+                {
+                    id: 0,
+                    question: '...Would you rather live in a castle or a spaceship traveling far from earth?',
+                    answer1: 'a castle',
+                    answer2: 'a spaceship',
+                },
+                {
+                    id: 1,
+                    question: '...Would you rather be able to control fire or water?',
+                    answer1: 'fire',
+                    answer2: 'water'
+                },
+                {
+                    id: 2,
+                    question: '...Would you rather go skydiving or scubadiving?',
+                    answer1: 'skydiving',
+                    answer2: 'scubadiving'
+                }
+            ],
+            answers: ['', '', ''],
+            answer: '',
+            filled: false
+        }
+    },
+    methods: {
+        questionAnswered(answerId, answer) {
+            this.$set(this.answers, answerId, answer)
+        }
+    }
+
+}
+</script>
+
+<style>
+#app {
+}
+.title {
+    text-align: center;
+}
+
+.body {
+    background-color: #F5FAFF
+}
+</style>
---
+++ src/components/WouldYouRatherQuestion.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <div id="card" class="card shadow m-2 p-2">
+            <h2 class="card-text">{{ question }}</h2>
+            <div class="card-body">
+                <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                    <label class="btn btn-secondary">
+                        <input 
+                        type="radio" 
+                        v-model="choice" 
+                        v-bind:value="answer1" 
+                        v-on:change="questionAnswered(choice)">
+                        {{ answer1 }}
+                    </label>
+                    
+                    <label class="btn btn-secondary">
+                        <input 
+                        type="radio" 
+                        v-model="choice" 
+                        v-bind:value="answer2" 
+                        v-on:change="questionAnswered(choice)">
+                        {{ answer2 }}
+                    </label>
+                </div>
+            </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+    export default {
+        name: 'WouldYouRatherQuestion',
+        data() {        /* Data - for internal data that belongs to this component */
+            return {
+                choice: ''
+            }
+        },
+        props: {        /* Props - for data passed in from parent component */
+            id: Number,
+            question: String,
+            answer1: String,
+            answer2: String,
+            
+        },
+        methods: {
+            questionAnswered(choice) {
+                console.log(`Answer changed: ${choice}`)
+                this.$emit('answer-changed', this.id, choice)
+            }
+        }
+    }
+</script>
+
+<style>
+#card{
+    background-color: aliceblue;
+}
+</style>
+
